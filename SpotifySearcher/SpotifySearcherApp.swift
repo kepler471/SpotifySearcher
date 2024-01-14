f1c13be1442b6eebfd26e103301f7d4bc1fa1473
Stelios Georgiou <steliosdg@gmail.com>
2024-01-14 13:59:49 +0000

    Initial Commit

---
+++ SpotifySearcher/SpotifySearcherApp.swift
@@ -0,0 +1,33 @@
+//
+//  SpotifySearcherApp.swift
+//  SpotifySearcher
+//
+//  Created by Stelios Georgiou on 12/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpotifySearcherApp: App {
+//    @State var currentNumber: String = "1"
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+//        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+//            // 3
+//            Button("One") {
+//                currentNumber = "1"
+//            }.padding()
+//            Button("Two") {
+//                currentNumber = "2"
+//            }.padding()
+//            Button("Three") {
+//                currentNumber = "3"
+//            }.padding()
+//        }
+//        .menuBarExtraStyle(.window)
+    }
+}
