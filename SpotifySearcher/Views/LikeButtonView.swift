92f88b3cfb2fe93c87c16a7657398b263c64a5ca
Stelios Georgiou <steliosdg@gmail.com>
2024-02-20 20:31:03 +0000

    extract to file

--- SpotifySearcher/Views/LikeButtonView.swift
+++ SpotifySearcher/Views/LikeButtonView.swift
@@ -8,10 +8,54 @@
 import SwiftUI
 
 struct LikeButtonView: View {
+    @EnvironmentObject var auth: Auth
+    @EnvironmentObject var player: Player
+    @State var isSaved: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+//        VStack {
+//            Button(action: {}, label: {
+//                Image(systemName: "leaf.fill")
+//            })
+//        }.accentColor(Color(.systemPink))
+        
+        Button {
+            if isSaved {
+                MySpotifyAPI.shared.removeTracksFromLibrary(accessToken: auth.accessToken, trackIds: [player.currentTrack!.id]) { error in
+                    if let error {
+                        print("<<<Like>>> 💔 Error trying to remove track from library: \(error)")
+                    }
+                }
+            } else {
+                MySpotifyAPI.shared.saveTracksToLibrary(accessToken: auth.accessToken, trackIds: [player.currentTrack!.id]) { error in
+                    if let error {
+                        print("<<<Like>>> ❤️ Error trying to save track to library: \(error)")
+                    }
+                }
+            }
+            isSaved.toggle()
+        } label: {
+            isSaved ? Image(systemName: "heart.fill") : Image(systemName: "heart")
+        }
+//        .foregroundStyle(.red)
+//        .backgroundStyle(.blue)
+//        .tint(.purple)
+        .scaleEffect(1)
+        .animation(.linear(duration: 1), value: 1)
+        .onReceive(player.$currentTrack) { _ in
+            MySpotifyAPI.shared.checkSaved(accessToken: auth.accessToken, type: "track", Ids: [player.currentTrack!.id]) { result, error  in
+                if let error {
+                    print("<<<Like>>> 🔎❤️ Error checking if track is saved in Library: \(error)")
+                } else if let result {
+                    isSaved = result.first!
+                }
+            }
+        }
+        .keyboardShortcut("s")
     }
 }
+
 
 #Preview {
     LikeButtonView()
